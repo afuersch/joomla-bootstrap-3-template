885d9db3ca9ba4546bd692670302f7680712e8f3
afuersch <adrian.fuerschuss@gmx.net>
2014-04-18 18:31:08 +0200

    overwrite articles latest and news template.

---
+++ html/mod_articles_news/horizontal.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_articles_news
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+
+<div class="row newsflash-horiz<?php echo $params->get('moduleclass_sfx'); ?>">
+<?php $columnSize = 12 / count($list); ?>
+<?php for ($i = 0, $n = count($list); $i < $n; $i ++) :
+	$item = $list[$i]; ?>    
+    <div class="highlight <?php echo "col-md-" . $columnSize; ?>">
+	<?php require JModuleHelper::getLayoutPath('mod_articles_news', '_item');
+
+	if ($n > 1 && (($i < $n - 1) || $params->get('showLastSeparator'))) : ?>
+
+	<span class="article-separator">&#160;</span>
+
+	<?php endif; ?>
+    </div>
+<?php endfor; ?>
+</div>
